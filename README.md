1dc46cdce3ab147ac914da5d3fff05956d0b95d2
Ekaterina Akhmetzyanova <a.ekaterina323@gmail.com>
2021-05-13 01:25:35 +0300

    arr buffer task

---
+++ README.md
@@ -0,0 +1,2 @@
+Домашнее задание к лекции «ArrayBuffer». Array Buffer  
+[![Build status](https://ci.appveyor.com/api/projects/status/r60111c0m9vw1qrb?svg=true)](https://ci.appveyor.com/project/EkaterinaAkhmetzyanova/ajs-hw-3-2-arraybuffer-task2)
---
+++ src/js/__tests__/ArrayBufferConverter.test.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-undef */
+import ArrayBufferConverter from '../ArrayBufferConverter';
+import getBuffer from '../getBuffer';
+
+test('check converter', () => {
+  const arrayBufferConverter = new ArrayBufferConverter();
+  const expected = '{"data":{"user":{"id":1,"name":"Hitman","level":10}}}';
+  arrayBufferConverter.load(getBuffer());
+  expect(arrayBufferConverter.toString()).toBe(expected);
+});
